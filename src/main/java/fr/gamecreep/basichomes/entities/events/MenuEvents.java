56b8ded1163553ec88d0976258ffd031ccf1d5db
iGameCreep <gamecreepbs@gmail.com>
2023-06-10 22:32:11 +0200

    V1.2: Finished UI for /homes

---
+++ src/main/java/fr/gamecreep/basichomes/entities/events/MenuEvents.java
@@ -0,0 +1,73 @@
+package fr.gamecreep.basichomes.entities.events;
+
+import fr.gamecreep.basichomes.BasicHomes;
+import fr.gamecreep.basichomes.entities.homes.PlayerHome;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Objects;
+
+public class MenuEvents implements Listener {
+    private final BasicHomes plugin;
+
+    public MenuEvents(BasicHomes plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        Player player = (Player) event.getWhoClicked();
+        Inventory inv = event.getClickedInventory();
+        ItemStack item = event.getCurrentItem();
+        int page = Integer.parseInt(inv.getItem(49).getItemMeta().getDisplayName().replaceAll("[^0-9/]+", "").split("/")[0]);
+
+        // The inventory is the homes menu
+        if (event.getView().getTitle().equals("§bMy Homes")) {
+            player.closeInventory();
+            switch (Objects.requireNonNull(item).getItemMeta().getDisplayName()) {
+                case "§6<-- Previous Page":
+                    plugin.getHomesUtils().openHomeInventory(player, page - 1);
+                    break;
+                case "§6Next Page -->":
+                    plugin.getHomesUtils().openHomeInventory(player, page + 1);
+                    break;
+                case "§cDelete this home":
+                    String home_name = item.getItemMeta().getLore().get(0);
+                    PlayerHome home;
+                    try {
+                        home = plugin.getHomeByName(player, home_name);
+                    } catch (Error err) {
+                        plugin.getChatUtils().sendPlayerError(player, "Could not delete the home.");
+                        break;
+                    }
+                    plugin.removeHome(home);
+                    plugin.getChatUtils().sendPlayerInfo(player, "Home §e" + home_name + "§b has been removed !");
+                    break;
+                default:
+                    if (item.getType().equals(Material.ENDER_EYE)) {
+                        // Item clicked is the page indicator
+                        plugin.getHomesUtils().openHomeInventory(player, page);
+                        break;
+                    }
+                    // Item clicked is a home
+			        String name = item.getItemMeta().getDisplayName();
+                    PlayerHome playerHome;
+                    try {
+                        playerHome = plugin.getHomeByName(player, name);
+                    } catch (Error err) {
+                        plugin.getChatUtils().sendPlayerError(player, "Could not get the home.");
+                        break;
+                    }
+                    player.teleport(playerHome.getLocation());
+                    plugin.getChatUtils().sendPlayerInfo(player, "Teleporting you to §e" + playerHome.getHomeName() + "§b...");
+			        break;
+            }
+        }
+    }
+
+}
---
+++ src/main/java/fr/gamecreep/basichomes/utils/HomesUtils.java
@@ -0,0 +1,122 @@
+package fr.gamecreep.basichomes.utils;
+
+import fr.gamecreep.basichomes.BasicHomes;
+import fr.gamecreep.basichomes.entities.homes.PlayerHome;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomesUtils {
+    private final BasicHomes plugin;
+
+    public HomesUtils (BasicHomes plugin) {
+        this.plugin = plugin;
+    }
+
+    public void openHomeInventory(Player player, int currentPage) {
+        final int homesPerPage = 4;
+        List<PlayerHome> homes = plugin.getAllPlayerHomes(player);
+
+        int totalPages = (int) Math.ceil((double) homes.size() / homesPerPage);
+
+        Inventory inventory = Bukkit.createInventory(null, 54, "§bMy Homes");
+
+        int startIndex = (currentPage - 1) * homesPerPage;
+        int endIndex = Math.min(startIndex + homesPerPage, homes.size());
+
+        for (int i = startIndex; i < endIndex; i++) {
+            PlayerHome home = homes.get(i);
+
+            ItemStack homeItem = createHomeItem(home);
+            int homeItemSlot = 10 + ((i - startIndex) * 9);
+            inventory.setItem(homeItemSlot, homeItem);
+
+            ItemStack delItem = createDelHomeItem(home);
+            int delItemSlot = 16 + ((i - startIndex) * 9);
+            inventory.setItem(delItemSlot, delItem);
+        }
+
+        addPaginationButtons(inventory, currentPage, totalPages);
+
+        player.openInventory(inventory);
+    }
+
+    private void addPaginationButtons(Inventory inventory, int currentPage, int totalPages) {
+        ItemStack prevPageItem = createNavigationItem("§6<-- Previous Page");
+        ItemStack nextPageItem = createNavigationItem("§6Next Page -->");
+
+        if (currentPage > 1) {
+            inventory.setItem(48, prevPageItem);
+        }
+        if (currentPage < totalPages) {
+            inventory.setItem(50, nextPageItem);
+        }
+
+        ItemStack pageIndicatorItem = createPageIndicatorItem(currentPage, totalPages);
+        inventory.setItem(49, pageIndicatorItem);
+    }
+
+    private ItemStack createNavigationItem(String displayName) {
+        ItemStack item = new ItemStack(Material.GREEN_STAINED_GLASS_PANE);
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName(displayName);
+        item.setItemMeta(meta);
+        return item;
+    }
+
+    private ItemStack createPageIndicatorItem(int currentPage, int totalPages) {
+        ItemStack item = new ItemStack(Material.ENDER_EYE);
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName("Page " + currentPage + "/" + totalPages);
+        item.setItemMeta(meta);
+        return item;
+    }
+
+    private ItemStack createHomeItem(PlayerHome home) {
+        Material material;
+
+        switch (home.getWorld()) {
+            case "world":
+                material = Material.GRASS_BLOCK;
+                break;
+            case "world_nether":
+                material = Material.CRIMSON_NYLIUM;
+                break;
+            case "world_the_end":
+                material = Material.END_PORTAL_FRAME;
+                break;
+            default:
+                material = Material.ENDER_PEARL;
+                break;
+        }
+
+        ItemStack homeItem = new ItemStack(material);
+        List<String> lore = new ArrayList<>();
+
+        ItemMeta homeItemMeta = homeItem.getItemMeta();
+        homeItemMeta.setDisplayName(home.getHomeName());
+        lore.add("Click to teleport!");
+        homeItemMeta.setLore(lore);
+        homeItem.setItemMeta(homeItemMeta);
+
+        return homeItem;
+    }
+    private ItemStack createDelHomeItem(PlayerHome home) {
+        ItemStack delItem = new ItemStack(Material.BARRIER);
+        List<String> lore = new ArrayList<>();
+
+        ItemMeta delItemMeta = delItem.getItemMeta();
+        delItemMeta.setDisplayName("§cDelete this home");
+        lore.add(home.getHomeName());
+        delItemMeta.setLore(lore);
+        delItem.setItemMeta(delItemMeta);
+
+        return delItem;
+    }
+}
