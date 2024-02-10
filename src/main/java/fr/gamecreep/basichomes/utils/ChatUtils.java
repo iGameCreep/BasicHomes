6813591d117d48f581ba4f67f519702807af5500
iGameCreep <gamecreep35@gmail.com>
2024-02-10 13:38:42 +0100

    Removed database & accounts

--- src/main/java/fr/gamecreep/basichomes/utils/ChatUtils.java
+++ src/main/java/fr/gamecreep/basichomes/utils/ChatUtils.java
@@ -1,7 +1,5 @@
 package fr.gamecreep.basichomes.utils;
 
-import net.md_5.bungee.api.chat.ClickEvent;
-import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.ChatColor;
 import lombok.NonNull;
 import org.bukkit.entity.Player;
@@ -14,36 +12,5 @@ public class ChatUtils
 
     public void sendPlayerInfo(@NonNull final Player player, final String message) {
         player.sendMessage(ChatColor.AQUA + message);
-    }
-
-    public TextComponent generateCommandComponent(final String command, final boolean underlined, final net.md_5.bungee.api.ChatColor color) {
-        final TextComponent component = new TextComponent(command);
-        component.setColor(color);
-        component.setUnderlined(underlined);
-        component.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, command));
-        return component;
-    }
-
-    public TextComponent generateClipboardComponent(final String text, final String toCopy, final boolean underlined, final net.md_5.bungee.api.ChatColor color) {
-        final TextComponent component = new TextComponent(text);
-        component.setColor(color);
-        component.setUnderlined(underlined);
-        component.setClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, toCopy));
-        return component;
-    }
-
-    public TextComponent generateLinkComponent(final String text, final String url, final boolean underlined, final net.md_5.bungee.api.ChatColor color) {
-        final TextComponent component = new TextComponent(text);
-        component.setColor(color);
-        component.setUnderlined(underlined);
-        component.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, url));
-        return component;
-    }
-
-    public TextComponent textToComponent(final String text, final net.md_5.bungee.api.ChatColor color) {
-        final TextComponent component = new TextComponent(text);
-        component.setColor(color);
-        component.setUnderlined(Boolean.FALSE);
-        return component;
     }
 }
